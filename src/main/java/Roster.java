8246d07ee7c57f39d45332a72a7133858b8fa7a8
Enoch <walkerhaozheng@gmail.com>
2019-11-29 15:47:07 -0800

    Created Roster Class

---
+++ src/main/java/Roster.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Roster {
+
+    public String courseName;
+    public String courseNumber;
+    public List<Student> students;
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+
+    public Roster(String name, String number) {
+        this.courseName = name;
+        this.courseNumber = number;
+        this.students = new ArrayList<>();
+    }
+
+    public void addStudent(Student student) {
+        this.students.add(student);
+    }
+}
--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,6 +1,25 @@
+import java.util.*;
+
 public class Student {
+
+    private String name;
+    private List<Double> assignmentScores;
+    private List<Double> examScores;
 
     public static void main(String[] args) {
         System.out.println("test");
+    }
+
+    public Student(String name) {
+        this.name = name;
+        this.assignmentScores = new ArrayList<>();
+    }
+
+    public void addAssignmentScore(double score) {
+        this.assignmentScores.add(score);
+    }
+
+    public void addExamScore(double score) {
+        this.examScores.add(score);
     }
 }
