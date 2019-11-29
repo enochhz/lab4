a275999f51f3087323be2083a4e4974c5be6f218
Enoch <walkerhaozheng@gmail.com>
2019-11-29 15:31:59 -0800

    Project initialization

---
+++ src/test/java/StudentTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StudentTest {
+    @Test
+    public void firstTest() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void test2() {
+        Assert.assertFalse(true);
+    }
+}
